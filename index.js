99095120b4839e498f04a897fdf87ff64786e037
Sharon Watkins <swatkins876@gmail.com>
2020-03-21 14:19:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(arr) {
+    let newArr = []
+    for (let i = 0; i < arr.length; i++) {
+        newArr.push(`Thank you, ${arr[i]}, for the wonderful surprise gift!`);
+    }
+    return newArr;
+}
+
+function countDown() {
+    let num = 10
+    while (num >= 0) {
+        console.log(num--)
+    }
+}
